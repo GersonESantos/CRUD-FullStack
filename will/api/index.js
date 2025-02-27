8592dd3345e9a9b6d2bd5307362624cf93b35ce8
GersonESS <gebhsantos@gmail.com>
2025-02-27 09:11:05 -0300

    a

--- will/api/index.js
+++ will/api/index.js
@@ -3,15 +3,10 @@ import userRoutes from "./routes/users.js"
 import cors from "cors"
 
 const app = express()
+
 app.use(express.json())
 app.use(cors())
 
 app.use("/", userRoutes)
 
-
-
-
-
-app.listen(3000, () => {
-    console.log('🚀Rodando index.js v0 listening at http://localhost:3000');
-  });
+app.listen(8800)
