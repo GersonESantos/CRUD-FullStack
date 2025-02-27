d4ecf7d4a0c47bbbd2dcc482297a707c25cacf79
GersonESS <gebhsantos@gmail.com>
2025-02-27 08:44:32 -0300

    a

--- v0/api/index.js
+++ v0/api/index.js
@@ -1,12 +1,21 @@
 import express from 'express';
 import cors from 'cors';
+import userRoutes from './routes/users.js';
+
 const app = express();
 app.use(express.json());
 app.use(cors());
+// app.use('/', userRoutes);
 
-app.get('/', (req, res) => {
-  res.send('CRUD Full Stack com Node');
+app.get("/", (req, res) => {
+  connection.query("SELECT COUNT(*) users FROM users", (err, results) => {
+      if (err) {
+          res.send('MySQL connection error.');
+      }
+      res.send('numero de users: ' + results[0].users);
+  })
 });
+
 
 
 app.listen(3000, () => {
