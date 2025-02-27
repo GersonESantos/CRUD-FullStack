20201c6a176195aea2f225616fcbd698fb853f76
GersonESS <gebhsantos@gmail.com>
2025-02-27 02:15:05 -0300

    g

--- v0/api/index.js
+++ v0/api/index.js
@@ -1,12 +1,11 @@
 import express from 'express';
 import cors from 'cors';
 const app = express();
-
 app.use(express.json());
 app.use(cors());
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('CRUD Full Stack com Node');
 });
 
 
